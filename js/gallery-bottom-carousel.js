483ed6694644c130f38153b9c70e9920ea524217
Студент ІТ Академії ШАГ <student@ITStepVn.local>
2017-05-25 20:24:55 +0300

    at work

---
+++ js/gallery-bottom-carousel.js
@@ -0,0 +1,36 @@
+/**
+ * Created by igor7 on 08.05.2017.
+ */
+$(document).ready(function(){
+    $('#gallery-bottom-carousel').carousel({
+        interval: false
+    });
+    $('.gallery-bottom-carousel-main .item').each(function(){
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        for (var i=0;i<2;i++) {
+            next=next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
+    var totalItems = $('.gallery-bottom-carousel-main .item').length;
+    var currentIndex = $('div.active').index() + 1;
+    $('.num').html(''+currentIndex+'/'+totalItems+'');
+    $('#gallery-bottom-carousel .carousel-control.right').on('click', function () {
+        currentIndex = (currentIndex )%(totalItems)+1;
+        $('.num').html(''+currentIndex+'/'+totalItems+'');
+    });
+    $('#gallery-bottom-carousel .carousel-control.left').on('click', function () {
+        currentIndex = (currentIndex )%(totalItems)-1;
+        if(currentIndex<=0)
+            currentIndex = currentIndex + totalItems;
+        $('.num').html(''+currentIndex+'/'+totalItems+'');
+    });
+});
